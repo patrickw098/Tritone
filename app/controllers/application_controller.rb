7c414eeaff5c5671f7e394f3c3997441e09a57d1
Patrick W <patricw2@uci.edu>
2018-06-10 17:05:25 -0700

    Debug Heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
 
   def current_user
-    @current_user ||= User.find_by(session_token: cookies.encrypted[:session_token])
+    @current_user ||= User.find_by(session_token: session[:session_token])
   end
 
   def logged_in?
@@ -12,12 +12,12 @@ class ApplicationController < ActionController::Base
 
   def logout
     current_user.reset_session_token!
-    cookies.encrypted[:session_token] = nil
+    session[:session_token] = nil
   end
 
   def login(user)
     @current_user = user
-    cookies.encrypted[:session_token] = user.reset_session_token!
+    session[:session_token] = user.reset_session_token!
   end
 
 end
