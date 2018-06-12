1ae1d5bda9479c6f0c5399e260b9f55046285855
Patrick W <patricw2@uci.edu>
2018-06-12 11:23:29 -0700

    Push to github

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,10 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.web_socket_server_url = "wss://tritone.herokuapp.com/cable"
+
+  config.action_cable.allowed_request_origins = ['https://tritone.herokuapp.com', 'http://tritone.herokuapp.com']
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
