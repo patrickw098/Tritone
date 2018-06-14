5e4b50957dcf21e8fb25fce7b41b1e398ba5cfbe
Patrick W <patricw2@uci.edu>
2018-06-13 18:45:24 -0700

    Fix Loading Screen

--- frontend/components/greeting/get_started.jsx
+++ frontend/components/greeting/get_started.jsx
@@ -5,6 +5,11 @@ class GetStarted extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      username: null,
+      password: null
+    }
+
     this.getStarted = this.getStarted.bind(this);
     this.demoUser = this.demoUser.bind(this);
     this.redirectToChannel = this.redirectToChannel.bind(this);
@@ -17,6 +22,10 @@ class GetStarted extends React.Component {
 
   demoUser(e) {
     e.preventDefault();
+    this.setState({
+      username: "demo_user"
+    })
+
     this.props.loginCurrentUser({ username: "demo_user", password: "fireball42" })
   }
 
@@ -29,19 +38,35 @@ class GetStarted extends React.Component {
     const { currentUser } = this.props;
 
     if (!currentUser) {
-      return (
-        <div className="get-started-div">
-          <h1> Tritone, a discord app clone </h1>
-          <div className="get-started-button-container">
-            <button className="demo-user" onClick={this.demoUser}>
-              <h2>Demo User Log In</h2>
-            </button>
-            <button className="get-started-button" onClick={this.getStarted}>
-              <h2>Get Started!</h2>
-            </button>
+      if (this.state.username === null) {
+        return (
+          <div className="get-started-div">
+            <h1> Tritone, a discord app clone </h1>
+            <div className="get-started-button-container">
+              <button disabled={this.state.username} className="demo-user" onClick={this.demoUser}>
+                <h2>Demo User Log In</h2>
+              </button>
+              <button disabled={this.state.username} className="get-started-button" onClick={this.getStarted}>
+                <h2>Get Started!</h2>
+              </button>
+            </div>
           </div>
-        </div>
-      );
+        );
+      } else {
+        return (
+          <div className="get-started-div">
+            <h1> Tritone, a discord app clone </h1>
+            <div className="get-started-button-container">
+              <button disabled={this.state.username} id="logging-in" className="demo-user">
+                <h2>Logging In...</h2>
+              </button>
+              <button disabled={this.state.username} className="get-started-button">
+                <h2>Get Started!</h2>
+              </button>
+            </div>
+          </div>
+        );
+      }
     } else {
       return (
         <Redirect to="/app" />
